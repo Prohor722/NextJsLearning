1e4c673511f61347da30ba6c95a09ad17458a596
Prohor722 <prohor.b722@gmail.com>
2023-01-21 23:25:14 +0600

    first commit

---
+++ pages/prohor.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+const Prohor = () => {
+    return ( 
+        <div className={styles.main}>
+            <h1>Hello My Name is Prohor</h1> 
+        </div>
+    );
+}
+ 
+export default Prohor;
